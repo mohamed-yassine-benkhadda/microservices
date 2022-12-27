07604522fe5695e6fb67e0ffb1d50159bf4f670b
BENKHADDA Mohamed Yassine <Mybenkhadda@gmail.com>
2022-12-27 03:40:16 +0100

    Retrieve link from Eureka Done

--- Admin/src/main/java/com/microservices/admin/GestionAdmin.java
+++ Admin/src/main/java/com/microservices/admin/GestionAdmin.java
@@ -1,12 +1,19 @@
 package com.microservices.admin;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -15,6 +22,8 @@ public class GestionAdmin {
 
     @Autowired
     AdminRespository adminRespository;
+    @Autowired
+    private EurekaClient discoveryClient;
     @GetMapping("ajouter")
     public Admin addAdmin(@RequestParam("nom") String nom, @RequestParam("prenom") String prenom, @RequestParam("cin") String cin, @RequestParam("dateNaissance") String dateNaissance, @RequestParam("adresse") String adresse){
         Admin a = new Admin(cin,nom,prenom,dateNaissance,adresse);
@@ -38,13 +47,14 @@ public class GestionAdmin {
     }
 
     @GetMapping("test")
-    public void testVehicule(){
+    public ResponseEntity<String> testVehicule(){
         RestTemplate restTemplate = new RestTemplate();
-        int id = 3;
-//        List<Vehicule> listeVehicule = restTemplate.getForObject("http://localhost:8086/vehicule", Vehicule.class);
-//        for(Vehicule v:listeVehicule){
-//            System.out.println(v);
-//        }
-        System.out.println( restTemplate.getForObject("http://vehicule/vehicule/ajouter?marque=renault&model=symbol&categorie=sedan&permis=B&immatricule=26B96546", Vehicule.class));
+        List<InstanceInfo> apps = discoveryClient.getApplication("VEHICULE").getInstances();
+        String url = "http://"+apps.get(0).getHostName()+":"+apps.get(0).getPort()+"/"+apps.get(0).getAppName().toLowerCase();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <String> entity = new HttpEntity<String>(headers);
+        ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        return result;
     }
 }
---
+++ Admin/src/main/java/com/microservices/admin/VehiculeList.java
@@ -0,0 +1,18 @@
+package com.microservices.admin;
+
+import java.util.List;
+
+public class VehiculeList {
+    private List<Vehicule> listeVehicule;
+
+    public List<Vehicule> getListeVehicule() {
+        return listeVehicule;
+    }
+
+    public void setListeVehicule(List<Vehicule> listeVehicule) {
+        this.listeVehicule = listeVehicule;
+    }
+
+    public VehiculeList() {
+    }
+}
