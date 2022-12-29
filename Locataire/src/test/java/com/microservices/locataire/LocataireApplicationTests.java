1673316d535e3d4d246cc8cb5efb76b73c278cbb
BENKHADDA Mohamed Yassine <Mybenkhadda@gmail.com>
2022-12-29 23:21:22 +0100

    -

---
+++ Locataire/src/test/java/com/microservices/locataire/LocataireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.locataire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocataireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
