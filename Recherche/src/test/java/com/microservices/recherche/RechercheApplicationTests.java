7f822cf0d7cd4b5003ddb90e9684cb26eab5ad5d
BENKHADDA Mohamed Yassine <Mybenkhadda@gmail.com>
2022-12-30 03:52:40 +0100

    Recherche ✅

---
+++ Recherche/src/test/java/com/microservices/recherche/RechercheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.recherche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RechercheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Recherche_microservices/src/main/java/com/microservices/recherche_microservices/RechercheMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.recherche_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechercheMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RechercheMicroservicesApplication.class, args);
+    }
+
+}
